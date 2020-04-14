5502741492c1b034375330d983ae876c99e0cea2
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-04-14 18:33:07 +0700

    UI Login done

--- FoodDeliveryWebsite/frontend/src/views/Login.vue
+++ FoodDeliveryWebsite/frontend/src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
 <body>
   <div></div>
-  <b-container >
+  <b-container>
     <b-row>
       <b-col></b-col>
       <b-col class="mt-5">
         <h1 class="title">Log In</h1>
-        <p class ="title mt-5">Welcome Back, Guess</p>
+        <p class="title mt-5">Welcome Back, Guess</p>
         <div class="form">
           <b-form @submit="onSubmit" @reset="onReset" v-if="show">
             <b-form-group
@@ -14,38 +14,30 @@
               class="input-title"
               label="Email address:"
               label-for="input-1"
-              description="We'll never share your email with anyone else."
             >
               <b-form-input
                 id="login-email"
                 v-model="form.email"
                 type="email"
                 required
-                placeholder="Enter email"
+                placeholder="Email"
               ></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-2" class = "input-title" label="Your Name:" label-for="input-2">
-              <b-form-input id="input-2" v-model="form.name" required placeholder="Password"></b-form-input>
+            <b-form-group
+              id="input-group-1"
+              class="input-title"
+              label="Password:"
+              label-for="input-1"
+            >
+              <b-form-input id="login-password" v-model="form.password" type="password" placeholder ="Password" required ></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-              <b-form-select id="input-3" v-model="form.food" :options="foods" required></b-form-select>
-            </b-form-group>
-
-            <b-form-group id="input-group-4">
-              <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-                <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                <b-form-checkbox value="that">Check that out</b-form-checkbox>
-              </b-form-checkbox-group>
-            </b-form-group>
-
-            <b-button type="submit" variant="info">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-button block id="btn-login-submit" class = "mt-5" type="submit" variant="info">Submit</b-button>
           </b-form>
-          <b-card class="mt-3" header="Form Data Result">
+          <!-- <b-card class="mt-3" header="Form Data Result">
             <pre class="m-0">{{ form }}</pre>
-          </b-card>
+          </b-card>-->
         </div>
       </b-col>
       <b-col></b-col>
@@ -99,18 +91,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#app .form {
-  /* background-color: red;
-  z-index: 2; */
-}
-
 #app .title {
-  text-align:center;
-  color:white;
-}
-#app  .input-title{
+  text-align: center;
   color: white;
 }
+#app .input-title {
+  color: white;
+}
+
+#app #btn-login-submit {
+  background-color: #bf9e6b;
+}
+
 body {
   background-image: linear-gradient(
       to bottom,
