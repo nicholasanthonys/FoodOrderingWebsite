f9c40fbf09d777007191221315991a225c8e2211
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-04-14 19:28:54 +0700

    Add Home Page

--- FoodDeliveryWebsite/frontend/src/router/index.js
+++ FoodDeliveryWebsite/frontend/src/router/index.js
@@ -2,14 +2,20 @@ import Vue from 'vue';
 import VueRouter from 'vue-router'
 
 import Login from '@/views/Login'
+import Home from '@/views/Home'
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path : '/',
-        name : 'Home',
+        name : 'Login',
         component : Login
+    },
+    {
+        path :'/home',
+        name : 'Home',
+        component : Home
     }
 ]
 
