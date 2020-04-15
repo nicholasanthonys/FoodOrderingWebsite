e5f03dd09fb334d30ea25f6716b7c2c73958d716
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-04-15 18:46:07 +0700

    add footer component

---
+++ FoodDeliveryWebsite/frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="footer">
+        Copyright 2020
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Footer'
+    
+}
+</script>
+
+<style scoped>
+ #footer {
+     color : white;
+     text-align: center;
+ }
+</style>
