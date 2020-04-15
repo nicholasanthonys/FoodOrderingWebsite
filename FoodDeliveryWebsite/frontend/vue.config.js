1ce9e9eaa334323c8a3c986a18328318156b411d
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-04-15 11:32:46 +0700

    add vue.config.js

---
+++ FoodDeliveryWebsite/frontend/vue.config.js
@@ -0,0 +1,29 @@
+/* 
+    konfigurasi agar dapat memakai relative image
+    https://bootstrap-vue.js.org/docs/reference/images/
+
+*/
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options.transformAssetUrls = {
+            img: 'src',
+            image: 'xlink:href',
+            'b-avatar': 'src',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          }
+  
+          return options
+        })
+    }
+  }
