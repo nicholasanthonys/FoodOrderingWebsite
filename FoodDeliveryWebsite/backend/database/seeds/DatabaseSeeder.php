9378aae4c45d8218455c1d3b377509df0f69ff5a
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-20 23:04:11 +0700

    missing coma fixed in seeder

--- FoodDeliveryWebsite/backend/database/seeds/DatabaseSeeder.php
+++ FoodDeliveryWebsite/backend/database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProvinceSeeder::class,
-            CitySeeder::class
+            CitySeeder::class,
             MenuSeeder::class,
             OrderSeeder::class,
             OrderMenuSeeder::class
