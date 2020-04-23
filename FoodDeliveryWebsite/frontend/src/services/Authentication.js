720fcbf24c3ffb255885a3d6396402919ecdf224
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-23 16:32:43 +0700

    add function to register user

--- FoodDeliveryWebsite/frontend/src/services/Authentication.js
+++ FoodDeliveryWebsite/frontend/src/services/Authentication.js
@@ -1,9 +1,24 @@
 import axios from 'axios'
 
-export const authentication = async (form) => {
+export const loginUser = async (form) => {
     return axios
         .post("/api/login", {
             email: form.email,
             password: form.password
+        })
+}
+
+export const registerUser = async(form) => {
+    console.log('from axios form is');
+    console.log(form);
+    return axios
+        .post("/api/signup", {
+            name : form.name,
+            email : form.email,
+            password : form.password,
+            dob : form.dob,
+            address : form.address,
+            province : form.province,
+            city : form.city
         })
 }
