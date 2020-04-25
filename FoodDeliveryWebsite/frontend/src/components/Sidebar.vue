7ad3f3849551ef5bc1b443b98d69ff1ebbe9c523
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-04-25 18:07:22 +0700

    Made and Used Sidebar Component

---
+++ FoodDeliveryWebsite/frontend/src/components/Sidebar.vue
@@ -0,0 +1,81 @@
+<template>
+  <!-- Side Navbar -->
+  <div class="wrapper">
+    <nav id="sidebar">
+      <div class="sidebar-header">
+        <img class="logo" src="../assets/logo-kitchen-black.png" alt="Logo Kitchen" />
+      </div>
+
+      <ul class="list-unstyled components">
+        <li>
+          <a href="#" class="link-to-other" active>Pasta</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Steak</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Pizza</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Rice</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Soup</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Salad</a>
+        </li>
+        <li>
+          <a href="#" class="link-to-other">Drinks</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: stretch;
+  width: 15%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  z-index: 999;
+  background: #4a4a4a;
+  color: #fff;
+}
+
+#sidebar {
+  width: 100%;
+  height: 100%;
+}
+
+.link-to-other {
+  font-size: 16px;
+  color: #fff;
+}
+
+.link-to-other:hover .link-to-other:focus {
+  text-decoration: underline;
+  text-decoration-color: #fff;
+}
+
+.link-to-other:active .link-to-other:visited {
+  color: #ff9d9d;
+}
+
+li {
+  text-align: center;
+  margin-bottom: 41px;
+}
+
+.logo {
+  width: 80%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 80px;
+}
+</style>
--- FoodDeliveryWebsite/frontend/src/views/MenuPasta.vue
+++ FoodDeliveryWebsite/frontend/src/views/MenuPasta.vue
@@ -1,306 +1,143 @@
 <template>
-    <div class="menu-pasta">
-        <!-- Side Navbar -->
-        <div class="wrapper">
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <img class="logo" src="../assets/logo-kitchen-black.png" alt="Logo Kitchen" />
-                </div>
+  <div class="menu-pasta">
+      <!---Sidebar-->
+      <Sidebar/>
 
-                <ul class="list-unstyled components">
-                    <li>
-                        <a href="#" class="link-to-other" active>Pasta</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Steak</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Pizza</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Rice</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Soup</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Salad</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-to-other">Drinks</a>
-                    </li>
-                </ul>
-            </nav>
+    <div class="content">
+      <!-- Navbar -->
+      <Navbar id="navbar" />
+
+      <!-- Content of Menu -->
+      <div class="menu-row row">
+        <div class="menu" v-for="menu in menus" :key="menu.id">
+          <img class="menu-img" v-bind:src="menu.url_image" alt="Menu" />
+
+          <p class="menu-name">{{menu.name}}</p>
+          <p class="menu-price">Rp{{menu.price}}</p>
+
+          <button class="detail_menu" v-on:click="goToDetail">Detail Menu</button>
+          <button class="pesan">Pesan</button>
         </div>
-
-        <div class="content">
-            <!-- Navbar -->
-            <Navbar id="navbar"/>
-
-            <!-- Content of Menu -->
-            <div class="menu-row row">
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-            </div>
-
-            <div class="menu-row row">
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-                
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-            </div>
-
-            <div class="menu-row row">
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-                
-                <div class="menu">
-                    <img class="menu-img" src="../assets/bg_home_1.png" alt="Menu" />
-
-                    <p class="menu-name">Nasi Goreng Sosis</p>
-                    <p class="menu-price">Rp17.500</p>
-
-                    <button class="detail_menu">Detail Menu</button>
-                    <button class="pesan">Pesan</button>
-                </div>
-            </div>
-
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { getMenuPasta } from "@/services";
 export default {
- components : {
-     Navbar
- }   
-}
+  components: {
+    Navbar,
+    Sidebar
+  },
+  data: function() {
+    return {
+      menus: []
+    };
+  },
+  methods: {
+    fillMenus: async function() {
+      try {
+        let res = await getMenuPasta();
+        console.log("res data is ");
+        if (res.status >= 200 && res.status < 300) {
+          this.menus = res.data.menus;
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    goToDetail : function() {
+        this.$router.push('/detail');
+    }
+  },
+  mounted: function() {
+    this.fillMenus();
+  }
+};
 </script>
 
 <style scoped>
 .menu-pasta {
-    /*semetara font family poppins belum ada */
-    /* font-family: Poppins; */
-    width: auto;
-    height: auto;
-    padding-bottom: 20px;
-    background-color: #282828;
-}
-
-.wrapper {
-    display: flex;
-    align-items: stretch;
-    width: 15%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    z-index: 999;
-    background: #4A4A4A;
-    color: #fff;
-}
-
-#sidebar {
-    width: 100%;
-    height: 100%;
-}
-
-.link-to-other {
-    font-size: 16px;
-    color: #fff;
-}
-
-.link-to-other:hover .link-to-other:focus {
-    text-decoration: underline;
-    text-decoration-color: #fff;
-}
-
-.link-to-other:active .link-to-other:visited {
-    color: #FF9D9D;
-}
-
-li {
-    text-align: center;
-    margin-bottom: 41px;
-}
-
-.logo {
-    width: 80%;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 80px;
+  /*semetara font family poppins belum ada */
+  /* font-family: Poppins; */
+  width: auto;
+  height: auto;
+  padding-bottom: 20px;
+  background-color: #282828;
 }
 
 .content {
-    width: 85%;
-    margin-left: 15%;
+  width: 85%;
+  margin-left: 15%;
 }
 
 .menu-row {
-    margin-top: 55px;
-    margin-right: 0px;
-    margin-left: 0px;
+  margin-top: 55px;
+  margin-right: 0px;
+  margin-left: 0px;
 }
 
 .menu {
-    width: 18.75%;
-    height: auto;
-    padding-bottom: 18px;
-    min-height: 400px;
-    margin-left: 5%;
-    border-radius: 10px;
-    background-color: #fff; 
+  margin-bottom: 30px;
+  width: 18.75%;
+  height: auto;
+  padding-bottom: 18px;
+  min-height: 400px;
+  margin-left: 5%;
+  border-radius: 10px;
+  background-color: #fff;
 }
 
 .menu-img {
-    width: 100%;
-    height: 200px;
-    object-fit: fill;
-    border-radius: 10px 10px 0px 0px;
+  width: 100%;
+  height: 200px;
+  object-fit: fill;
+  border-radius: 10px 10px 0px 0px;
 }
 
 .menu-name {
-    margin-top: 11.5px;
-    margin-bottom: 0px;
-    font-style: bold;
-    font-size: 18px;
-    color: #0B3993;
-    text-align: center;
+  margin-top: 11.5px;
+  margin-bottom: 0px;
+  font-style: bold;
+  font-size: 18px;
+  color: #0b3993;
+  text-align: center;
 }
 
 .menu-price {
-    font-size: 14px;
-    margin-bottom: 25px;
-    color: #000;
-    text-align: center;
+  font-size: 14px;
+  margin-bottom: 25px;
+  color: #000;
+  text-align: center;
 }
 
 .detail_menu {
-    width: 80%;
-    max-width: 170px;
-    height: 45px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 10px;
-    color: #fff;
-    margin-bottom: 12px;
-    background-color: #275DC5;
-    box-shadow: 0px 2px rgba(0, 0, 0, 0.25);
+  width: 80%;
+  max-width: 170px;
+  height: 45px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+  color: #fff;
+  margin-bottom: 12px;
+  background-color: #275dc5;
+  box-shadow: 0px 2px rgba(0, 0, 0, 0.25);
 }
 
 .pesan {
-    width: 80%;
-    max-width: 170px;
-    height: 45px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 10px;
-    color: #fff;
-    background-color: #BF9E6B;
-    box-shadow: 0px 2px rgba(0, 0, 0, 0.25);
+  width: 80%;
+  max-width: 170px;
+  height: 45px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+  color: #fff;
+  background-color: #bf9e6b;
+  box-shadow: 0px 2px rgba(0, 0, 0, 0.25);
 }
-
 </style>
