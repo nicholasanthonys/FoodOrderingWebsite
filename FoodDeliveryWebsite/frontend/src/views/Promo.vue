c4d0fed98a4e02afcfc074def7005ff2988bd1cf
Alexander Tri Handoyo Wangsaputra <ATHWangsaputra@gmail.com>
2020-05-07 09:58:32 +0700

    Custom scrollbar

--- FoodDeliveryWebsite/frontend/src/views/Promo.vue
+++ FoodDeliveryWebsite/frontend/src/views/Promo.vue
@@ -568,4 +568,27 @@ export default {
   -o-transform: rotate(-5deg);
   -webkit-transform: rotate(-5deg);
 }
+
+/* Custom Scrollbar */
+/* width */
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #bf9e6b; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #9b7843;
+}
 </style>
